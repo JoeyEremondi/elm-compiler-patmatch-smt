29bf9a09369fb5e94a3b58775fbb553b6353f4bf
Joey Eremondi <joey.eremondi@gmail.com>
2019-05-01 10:43:33 -0700

    Add constr generator for patterns

--- compiler/src/Type/PatternMatch.hs
+++ compiler/src/Type/PatternMatch.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE PatternSynonyms #-}
 module Type.PatternMatch where
@@ -8,6 +10,8 @@ import           Control.Monad
 import Data.Foldable (foldrM)
 import qualified Data.Map.Strict as Map
 import Data.Word (Word32)
+
+import Data.Text (unpack)
 
 import qualified AST.Canonical as Can
 import qualified AST.Module.Name as ModuleName
@@ -50,6 +54,8 @@ data LitPattern =
     SetVar Variable
     | Ctor String [LitPattern] 
     | Proj Int LitPattern 
+    | Top
+    | Bottom 
 
 data Constraint =
     CAnd [Constraint]
@@ -61,7 +67,17 @@ data Constraint =
 p1 === p2 = 
     CSubset p1 p2 /\ CSubset p2 p1
 
-v1 << v2 = CSubset (SetVar v1) (SetVar v2)  
+class Subsetable a where
+    toLit :: a -> LitPattern
+     
+instance Subsetable LitPattern where
+    toLit = id 
+    
+instance Subsetable Variable where
+    toLit = SetVar
+
+(<<) :: (Subsetable a, Subsetable b ) => a -> b -> Constraint
+v1 << v2 = CSubset (toLit v1) (toLit v2)  
 
 instance Semigroup Constraint where
     CTrue <> c = c
@@ -155,8 +171,15 @@ constrainExpr tyMap _Gamma (A.At region expr)  =
     --TODO: for ctor, get its (simple) type scheme and instantiate
     --Turn into TypeEffect
     constrainExpr_ (Can.VarOperator expr1 expr2 expr3 expr4) t _Gamma = return CTrue --TODO built-in types for operators 
-    constrainExpr_ (Can.Binop expr1 expr2 expr3 expr4 expr5 expr6) t _Gamma = _ 
-    constrainExpr_ (Can.Case expr1 expr2) t _Gamma = _ 
+    constrainExpr_ (Can.Binop expr1 expr2 expr3 expr4 expr5 expr6) t _Gamma = return CTrue --TODO built-in types for operators  
+    constrainExpr_ (Can.Case discr branches) (discType :@ inputPatterns) _Gamma = do
+        let coveredPatterns = (_ :: LitPattern)
+        let coveredConstr =  inputPatterns << coveredPatterns
+        branchConstr <- CAnd <$> 
+            (forM branches $ 
+                \(Can.CaseBranch pat rhs) -> do
+                    return _ ) 
+        return $ coveredConstr /\ branchConstr --TODO safety constraint separately?
     constrainExpr_ (Can.Lambda expr1 expr2) ( (Fun (t1 :@ e1) (t2 :@ v2)) :@ v3 ) _Gamma = _ 
     constrainExpr_ (Can.Call fun args) (_ :@ retEffect) _Gamma = do
          (funTy, funConstr) <- self _Gamma fun 
@@ -170,8 +193,8 @@ constrainExpr tyMap _Gamma (A.At region expr)  =
                 argHelper (Fun (tdom :@ edom) cod :@ efun) (targ :@ earg : rest) accum = 
                     argHelper cod rest (earg << edom /\ accum ) 
     constrainExpr_ (Can.If expr1 expr2) t _Gamma = _ 
-    constrainExpr_ (Can.Let expr1 expr2) t _Gamma = _ 
-    constrainExpr_ (Can.LetRec expr1 expr2) t _Gamma = _ 
+    constrainExpr_ (Can.Let def inExpr)  t _Gamma = 
+        snd<$>constrainDef tyMap _Gamma def 
     constrainExpr_ (Can.LetDestruct expr1 expr2 expr3) t _Gamma = _ 
     constrainExpr_ (Can.Accessor expr) t _Gamma = _ 
     constrainExpr_ (Can.Access expr1 expr2) t _Gamma = _ 
@@ -179,9 +202,9 @@ constrainExpr tyMap _Gamma (A.At region expr)  =
     constrainExpr_ (Can.Record expr) t _Gamma = _ 
     constrainExpr_ (Can.Tuple expr1 expr2 expr3) t _Gamma = _ 
     constrainExpr_ (Can.List expr) t _Gamma = _ --TODO fold over list type 
-    constrainExpr_ (Can.Chr expr) t _Gamma = return CTrue
-    constrainExpr_ (Can.Str expr) t _Gamma = return CTrue
-    constrainExpr_ (Can.Int expr) t _Gamma =  return CTrue
+    constrainExpr_ (Can.Chr c) (_:@v) _Gamma = return $ (litChar c) << (SetVar v) 
+    constrainExpr_ (Can.Str s) t _Gamma = return CTrue
+    constrainExpr_ (Can.Int i) t _Gamma =  return CTrue
     constrainExpr_ (Can.Float expr) t _Gamma = return CTrue
     constrainExpr_ (Can.Negate expr) t _Gamma = return CTrue --TODO ints?
     constrainExpr_ (Can.VarKernel expr1 expr2) t _Gamma = return CTrue 
@@ -190,3 +213,48 @@ constrainExpr tyMap _Gamma (A.At region expr)  =
     constrainExpr_ Can.Unit t _Gamma = return CTrue 
     constrainExpr_ (Can.Shader expr1 expr2 expr3) t _Gamma = return CTrue     
     constrainExpr_ _ _ _ = error $ "Impossible type-expr combo "
+
+constrainDef :: Map.Map R.Region TypeEffect -> Gamma -> Can.Def ->   IO (TypeEffect,  Constraint)
+constrainDef tipes _Gamma def = _
+
+
+--Given a variable V for a pattern expression, and the LHS of a case branch,
+--Generate the constraint that the variable 
+canPatToLit ::  Can.Pattern -> LitPattern
+canPatToLit  (A.At info pat) = 
+    case pat of 
+        Can.PAnything -> Top
+        (Can.PVar x) -> _ Top
+        (Can.PRecord p) -> error "TODO records"
+        (Can.PAlias p1 p2) -> error "TODO Pattern alias" 
+        Can.PUnit -> litUnit 
+        (Can.PTuple p1 p2 (Just p3)) -> litTriple (canPatToLit p1) (canPatToLit p2) (canPatToLit p3)
+        (Can.PTuple p1 p2 Nothing) -> litPair (canPatToLit p1) (canPatToLit p2)
+        (Can.PList plist) -> litList (map canPatToLit plist)
+        (Can.PCons p1 p2) -> litCons (canPatToLit p1) (canPatToLit p2)
+        (Can.PBool _ b) -> if b then litTrue else litFalse 
+        (Can.PChr c) -> litChar c
+        (Can.PStr s) -> litString s
+        (Can.PInt i) -> litInt i
+        (Can.PCtor { Can._p_home = p__p_home, Can._p_type = p__p_type, Can._p_union = p__p_union, Can._p_name = ctorName, Can._p_index = p__p_index, Can._p_args = ctorArgs }) -> Ctor (N.toString ctorName) (map (canPatToLit . Can._arg) ctorArgs)  
+
+litUnit = Ctor "--Unit" []
+litPair l1 l2 = Ctor "--Pair" [l1, l2]
+litTriple l1 l2 l3 = Ctor "--Triple" [l1, l2, l3]
+litList l = case l of
+    [] -> litNull
+    (h : t) -> litCons h (litList t)
+litNull = Ctor "--Null" []
+litCons h t = Ctor "--Cons" [h, t]
+litTrue = Ctor "--True" []
+litFalse = Ctor "--False" []
+litChar c = Ctor ("--CHAR_" ++ unpack c) []
+litString s = Ctor ("--STRING" ++ show s) []
+litInt i = case i of
+    0 -> Ctor "--ZERO" []
+    _ | i < 0 -> Ctor "--NEGATIVE" [litNat (-i)]
+    _ | i > 0 -> Ctor "--POSITIVE" [litNat i]
+
+litNat n = case n of
+    0 -> Ctor "--ZERO" []
+    i | i > 0 -> Ctor "--SUCC" [litNat (i-1)]
