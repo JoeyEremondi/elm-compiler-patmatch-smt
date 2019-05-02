5b040a516df1ccd485204d01242fc74af1f9dc12
Joey Eremondi <joey.eremondi@gmail.com>
2019-05-01 21:46:29 -0700

    Draft of analysis for Case

--- compiler/src/Type/PatternMatch.hs
+++ compiler/src/Type/PatternMatch.hs
@@ -37,7 +37,7 @@ import Data.Monoid
 
 type Variable = UF.Point ()
 
-data TypeEffect_ = 
+data TypeEffect_ =
     PlaceHolder N.Name
     | Alias ModuleName.Canonical N.Name [(N.Name, TypeEffect)] TypeEffect
     | Var Variable
@@ -47,18 +47,18 @@ data TypeEffect_ =
     | Record (Map.Map N.Name TypeEffect) TypeEffect
     | Unit
     | Tuple TypeEffect TypeEffect (Maybe TypeEffect)
-    
 
 
-infix 9 :@    
-data TypeEffect =  TypeEffect_ :@ Variable  
 
-data LitPattern = 
+infix 9 :@
+data TypeEffect =  TypeEffect_ :@ Variable
+
+data LitPattern =
     SetVar Variable
-    | Ctor String [LitPattern] 
-    | Proj String Int LitPattern 
+    | Ctor String [LitPattern]
+    | Proj String Int LitPattern
     | Top
-    | Bottom 
+    | Bottom
     | Intersect LitPattern LitPattern
     | Union LitPattern LitPattern
 
@@ -68,28 +68,29 @@ data Constraint =
     | CImplies Constraint Constraint
     | CSubset LitPattern LitPattern
     | CTrue
+    | CNot Constraint
 
 newtype Safety = Safety [(Constraint, R.Region)]
-    deriving (Monoid, Semigroup) 
+    deriving (Monoid, Semigroup)
 
-p1 ==== p2 = 
+p1 ==== p2 =
     let l1 = toLit p1
         l2 = toLit p2
     in
     CSubset l1 l2 /\ CSubset l2 l1
 
 union :: (Subsetable a) => [a] -> LitPattern
-union = foldr (\ a b -> toLit a `Union` b) Bottom 
+union = foldr (\ a b -> toLit a `Union` b) Bottom
 
 intersect :: (Subsetable a) => [a] -> LitPattern
-intersect = foldr (\ a b -> toLit a `Intersect` b) Bottom 
+intersect = foldr (\ a b -> toLit a `Intersect` b) Bottom
 
 class Subsetable a where
     toLit :: a -> LitPattern
-     
+
 instance Subsetable LitPattern where
-    toLit = id 
-    
+    toLit = id
+
 instance Subsetable Variable where
     toLit = SetVar
 
@@ -97,12 +98,15 @@ instance Subsetable TypeEffect where
     toLit (_ :@ v) = toLit v
 
 (<<) :: (Subsetable a, Subsetable b ) => a -> b -> Constraint
-v1 << v2 = CSubset (toLit v1) (toLit v2)  
+v1 << v2 = CSubset (toLit v1) (toLit v2)
+
+(</<) :: (Subsetable a, Subsetable b ) => a -> b -> Constraint
+v1 </< v2 = CNot $ CSubset (toLit v1) (toLit v2)
 
 instance Semigroup Constraint where
     CTrue <> c = c
     c <> CTrue = c
-    c1 <> (CAnd cl) = CAnd (c1 : cl) 
+    c1 <> (CAnd cl) = CAnd (c1 : cl)
     c1 <> c2 = CAnd [c1, c2]
 
 instance Monoid Constraint where
@@ -137,26 +141,26 @@ c1 \/ c2 = COr [c1, c2]
 
 addEffectVars :: Type.Type -> IO TypeEffect
 addEffectVars t = do
-    (innerType :: TypeEffect_ ) <- 
+    (innerType :: TypeEffect_ ) <-
         case t of
-            (Type.PlaceHolder t) -> 
+            (Type.PlaceHolder t) ->
                 return $ PlaceHolder t
-            (Type.AliasN t1 t2 t3 t4) -> 
+            (Type.AliasN t1 t2 t3 t4) ->
                 error "TODO type aliases" -- (AliasN  t1 t2 ) <$> addEffectVars t3 <*> addEffectVars t4
-            (Type.VarN t) -> 
+            (Type.VarN t) ->
                 return $ Var (error "TODO var conversion")
-            (Type.AppN t1 t2 t3) -> 
+            (Type.AppN t1 t2 t3) ->
                 (App t1 t2) <$> (mapM addEffectVars  t3)
-            (Type.FunN t1 t2) -> 
+            (Type.FunN t1 t2) ->
                 Fun <$> addEffectVars t1 <*> addEffectVars t2
             Type.EmptyRecordN -> return EmptyRecord
-            (Type.RecordN t1 t2) -> 
+            (Type.RecordN t1 t2) ->
                 Record <$> (mapM addEffectVars t1) <*> addEffectVars t2
             Type.UnitN -> return Unit
-            (Type.TupleN t1 t2 t3) -> do 
+            (Type.TupleN t1 t2 t3) -> do
                 mt3 <- case t3 of
                     Nothing -> return Nothing
-                    Just t -> Just <$> addEffectVars t 
+                    Just t -> Just <$> addEffectVars t
                 Tuple <$> addEffectVars t1 <*> addEffectVars t2 <*> (return mt3)
     constraintVar <- UF.fresh ()
     return $ innerType :@ constraintVar
@@ -174,7 +178,7 @@ unifyTypes (_ :@ v1) (_ :@ v2) =
     --TODO do we need to unify sub-vars?
 
 
-type ConstrainM m = (MonadIO m, MonadWriter Safety m ) 
+type ConstrainM m = (MonadIO m, MonadWriter Safety m )
 
 tellSafety x = tell $ Safety [x]
 
@@ -185,53 +189,63 @@ tellSafety x = tell $ Safety [x]
 --Traverse that expression and generate the constraints that, when solved,
 -- give the possible patterns for each effect variable.
 -- Emits "safety constraints" using the Writer monad
-constrainExpr :: (ConstrainM m) => Map.Map R.Region TypeEffect -> (Gamma, Constraint) -> Can.Expr ->   m (TypeEffect,  Constraint) 
-constrainExpr tyMap _GammaPath (A.At region expr)  =  
+constrainExpr :: (ConstrainM m) => Map.Map R.Region TypeEffect -> (Gamma, Constraint) -> Can.Expr ->   m (TypeEffect,  Constraint)
+constrainExpr tyMap _GammaPath (A.At region expr)  =
     case Map.lookup region tyMap of
-        Just ty -> do 
+        Just ty -> do
             c <- constrainExpr_ expr ty _GammaPath
             return (ty, c)
         Nothing -> error "Region not in type map"
   where
-    self :: (ConstrainM m) => (Gamma, Constraint) -> Can.Expr ->   m (TypeEffect,  Constraint) 
-    self  = constrainExpr tyMap 
-    constrainExpr_ ::  (ConstrainM m) => Can.Expr_ -> TypeEffect -> (Gamma, Constraint) -> m Constraint 
+    self :: (ConstrainM m) => (Gamma, Constraint) -> Can.Expr ->   m (TypeEffect,  Constraint)
+    self  = constrainExpr tyMap
+    constrainExpr_ ::  (ConstrainM m) => Can.Expr_ -> TypeEffect -> (Gamma, Constraint) -> m Constraint
     constrainExpr_ (Can.VarLocal name) t (_Gamma, _) = do
         let (tipe, constr) = instantiate (_Gamma Map.! name)
         liftIO $ unifyTypes t tipe
         return CTrue
     constrainExpr_ (Can.VarTopLevel expr1 expr2) t _GammaPath = error "TODO get type from imports"
-    constrainExpr_ (Can.VarCtor expr1 expr2 expr3 expr4 expr5) t _GammaPath = _ 
+    constrainExpr_ (Can.VarCtor expr1 expr2 expr3 expr4 expr5) t _GammaPath = _
     --TODO: for ctor, get its (simple) type scheme and instantiate
     --Turn into TypeEffect
     constrainExpr_ (Can.VarOperator expr1 expr2 expr3 expr4) t _GammaPath = return CTrue --TODO built-in types for operators 
     constrainExpr_ (Can.Binop expr1 expr2 expr3 expr4 expr5 expr6) t _GammaPath = return CTrue --TODO built-in types for operators  
-    constrainExpr_ (Can.Case discr branches) inputPatterns (_Gamma, pathConstr) = do
-        let litBranches = map (\ (Can.CaseBranch pat rhs) -> (canPatToLit pat, rhs) ) branches
+    constrainExpr_ (Can.Case discr branches) resultType (_Gamma, pathConstr) = do
+        (inputPatterns, inputConstrs) <- self (_Gamma, pathConstr) discr
+        let litBranches = map (\ (Can.CaseBranch pat rhs) -> (pat, canPatToLit pat, rhs) ) branches
         --Emit a safety constraint: must cover all possible inputs by our branch patterns
-        tellSafety (pathConstr ==> (inputPatterns << union (map fst litBranches)), region)
-        branchConstr <- CAnd <$> 
-            (forM litBranches $ 
-                \(lit, rhs) -> do
-                    return _ ) 
-        return $  branchConstr --TODO safety constraint separately?
-    constrainExpr_ (Can.Lambda expr1 expr2) ( (Fun (t1 :@ e1) (t2 :@ v2)) :@ v3 ) _GammaPath = _ 
+        tellSafety (pathConstr ==> (inputPatterns << union (map (\(_,b,_) -> b) litBranches)), region)
+        (patVars, branchConstrs) <- unzip <$> 
+            forM litBranches (
+                \(pat, lit, rhs) -> do
+                    v <- liftIO $ UF.fresh ()
+                    let canBeInBranch = (intersect [toLit inputPatterns, lit]) </< Bottom
+                    let newEnv = envAfterMatch tyMap (toLit inputPatterns) pat
+                    let newPathConstr = canBeInBranch /\ pathConstr
+                    (rhsTy, rhsConstrs) <- self (Map.union newEnv _Gamma, newPathConstr) rhs
+                    return (v, 
+                        (canBeInBranch ==> (v ==== rhsTy)) 
+                        /\ ((CNot canBeInBranch) ==> (v ==== Bottom))))
+        --The result of the whole thing is the union of all the results of the branches
+        let resultConstr = resultType ==== (union patVars)
+        return (inputConstrs /\ resultConstr /\ CAnd branchConstrs) 
+    constrainExpr_ (Can.Lambda expr1 expr2) ( (Fun (t1 :@ e1) (t2 :@ v2)) :@ v3 ) _GammaPath = _
     constrainExpr_ (Can.Call fun args) retEffect _GammaPath = do
-         (funTy, funConstr) <- self _GammaPath fun 
-         (argTEs, argConstrs) <- unzip <$> mapM (self _GammaPath) args  
-         return $ argHelper funTy argTEs ( funConstr /\ CAnd argConstrs) 
+         (funTy, funConstr) <- self _GammaPath fun
+         (argTEs, argConstrs) <- unzip <$> mapM (self _GammaPath) args
+         return $ argHelper funTy argTEs ( funConstr /\ CAnd argConstrs)
             where
                 --Loop to compute the return type
                 --At each application, instantiate the function's argument type to be the type of the given argument
                 --Finally, make sure the patterns of the whole expression is the pattern the function returns
                 argHelper funEffect [] accum = funEffect ==== retEffect  /\ accum
-                argHelper (Fun (tdom :@ edom) cod :@ efun) (targ :@ earg : rest) accum = 
-                    argHelper cod rest (earg ==== edom /\ accum ) 
-    constrainExpr_ (Can.If expr1 expr2) t _GammaPath = _ 
-    constrainExpr_ (Can.Let def inExpr)  t _GammaPath = 
-        snd<$>constrainDef tyMap _GammaPath def 
-    constrainExpr_ (Can.LetDestruct expr1 expr2 expr3) t _GammaPath = _ 
-    constrainExpr_ (Can.Accessor expr) t _GammaPath = error "TODO records" 
+                argHelper (Fun (tdom :@ edom) cod :@ efun) (targ :@ earg : rest) accum =
+                    argHelper cod rest (earg ==== edom /\ accum )
+    constrainExpr_ (Can.If expr1 expr2) t _GammaPath = _
+    constrainExpr_ (Can.Let def inExpr)  t _GammaPath =
+        snd<$>constrainDef tyMap _GammaPath def
+    constrainExpr_ (Can.LetDestruct expr1 expr2 expr3) t _GammaPath = _
+    constrainExpr_ (Can.Accessor expr) t _GammaPath = error "TODO records"
     constrainExpr_ (Can.Access expr1 expr2) t _GammaPath = error "TODO records"
     constrainExpr_ (Can.Update expr1 expr2 expr3) t _GammaPath = error "TODO records"
     constrainExpr_ (Can.Record expr) t _GammaPath = error "TODO records"
@@ -243,7 +257,7 @@ constrainExpr tyMap _GammaPath (A.At region expr)  =
         (elemEffect1, constr1) <- self _GammaPath expr1
         (elemEffect2, constr2) <- self _GammaPath expr2
         (elemEffect3, constr3) <- self _GammaPath expr3
-        return $ CAnd [constr1, constr2, constr3, (litTriple elemEffect1 elemEffect2 elemEffect3) ==== vTuple]  
+        return $ CAnd [constr1, constr2, constr3, (litTriple elemEffect1 elemEffect2 elemEffect3) ==== vTuple]
     constrainExpr_ (Can.List expr) t _GammaPath = do
         (typeEffects, constrs) <- unzip <$> forM expr (self _GammaPath)
         return $ (CAnd constrs) /\ (t ==== litList typeEffects )
@@ -252,11 +266,11 @@ constrainExpr tyMap _GammaPath (A.At region expr)  =
     constrainExpr_ (Can.Int i) typeEffect  _GammaPath =  return $ (litInt i) ==== typeEffect
     constrainExpr_ (Can.Float expr) t _GammaPath = return CTrue
     constrainExpr_ (Can.Negate expr) t _GammaPath = error "TODO negation"
-    constrainExpr_ (Can.VarKernel expr1 expr2) t _GammaPath = return CTrue 
+    constrainExpr_ (Can.VarKernel expr1 expr2) t _GammaPath = return CTrue
     constrainExpr_ (Can.VarForeign expr1 expr2 expr3) t _GammaPath = return CTrue
     constrainExpr_ (Can.VarDebug expr1 expr2 expr3) t _GammaPath = return CTrue
-    constrainExpr_ Can.Unit (_:@v) _GammaPath = return $ litUnit ==== v 
-    constrainExpr_ (Can.Shader expr1 expr2 expr3) t _GammaPath = return CTrue     
+    constrainExpr_ Can.Unit (_:@v) _GammaPath = return $ litUnit ==== v
+    constrainExpr_ (Can.Shader expr1 expr2 expr3) t _GammaPath = return CTrue
     constrainExpr_ _ _ _ = error $ "Impossible type-expr combo "
 
 constrainDef ::  (ConstrainM m) => Map.Map R.Region TypeEffect -> (Gamma, Constraint) -> Can.Def ->   m (TypeEffect,  Constraint)
@@ -266,35 +280,35 @@ constrainDef tipes _GammaPath def = _
 --Given a variable V for a pattern expression, and the LHS of a case branch,
 --Generate the constraint that the variable 
 canPatToLit ::  Can.Pattern -> LitPattern
-canPatToLit  (A.At info pat) = 
-    case pat of 
+canPatToLit  (A.At info pat) =
+    case pat of
         Can.PAnything -> Top
         (Can.PVar x) -> _ Top
         (Can.PRecord p) -> error "TODO records"
-        (Can.PAlias p1 p2) -> error "TODO Pattern alias" 
-        Can.PUnit -> litUnit 
+        (Can.PAlias p1 p2) -> error "TODO Pattern alias"
+        Can.PUnit -> litUnit
         (Can.PTuple p1 p2 (Just p3)) -> litTriple (canPatToLit p1) (canPatToLit p2) (canPatToLit p3)
         (Can.PTuple p1 p2 Nothing) -> litPair (canPatToLit p1) (canPatToLit p2)
         (Can.PList plist) -> litList (map canPatToLit plist)
         (Can.PCons p1 p2) -> litCons (canPatToLit p1) (canPatToLit p2)
-        (Can.PBool _ b) -> if b then litTrue else litFalse 
+        (Can.PBool _ b) -> if b then litTrue else litFalse
         (Can.PChr c) -> litChar c
         (Can.PStr s) -> litString s
         (Can.PInt i) -> litInt i
-        (Can.PCtor { Can._p_name = ctorName, Can._p_args = ctorArgs }) -> Ctor (N.toString ctorName) (map (canPatToLit . Can._arg) ctorArgs)  
+        (Can.PCtor { Can._p_name = ctorName, Can._p_args = ctorArgs }) -> Ctor (N.toString ctorName) (map (canPatToLit . Can._arg) ctorArgs)
 
-envAfterMatch :: Map.Map R.Region TypeEffect ->  LitPattern  -> Can.Pattern -> Gamma  
+envAfterMatch :: Map.Map R.Region TypeEffect ->  LitPattern  -> Can.Pattern -> Gamma
 envAfterMatch typesForRegions matchedPat (A.At region pat)  =
-    let 
+    let
         ourType = typesForRegions Map.! region
-        ctorProjectionEnvs  topPat nameString ctorArgPats = 
+        ctorProjectionEnvs  topPat nameString ctorArgPats =
             let
-                subPats = map ((flip $ Proj nameString) topPat) [1.. (length ctorArgPats)] 
+                subPats = map ((flip $ Proj nameString) topPat) [1.. (length ctorArgPats)]
                 subDicts = zipWith ((envAfterMatch typesForRegions) ) subPats ctorArgPats
              in Map.unions subDicts
     in case pat of
         Can.PVar x -> Map.insert x (monoscheme ourType matchedPat) _Gamma
-        (Can.PCtor { Can._p_name = ctorName, Can._p_args = ctorArgs }) -> 
+        (Can.PCtor { Can._p_name = ctorName, Can._p_args = ctorArgs }) ->
             ctorProjectionEnvs matchedPat (N.toString ctorName) $ map Can._arg ctorArgs
         (Can.PTuple p1 p2 (Just p3)) -> ctorProjectionEnvs matchedPat ctorTriple [p1, p2, p3]
         (Can.PTuple p1 p2 Nothing) -> ctorProjectionEnvs matchedPat ctorPair [p1, p2]
@@ -304,7 +318,7 @@ envAfterMatch typesForRegions matchedPat (A.At region pat)  =
         (Can.PCons p1 p2) -> ctorProjectionEnvs matchedPat ctorCons [p1, p2]
         _ -> _Gamma
 
-                    
+
 
 --Helpers for generating literal patterns from builtin data types
 ctorUnit = "--Unit"
